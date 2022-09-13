d2dfb308a155fb57a186d5cb96f54032c704fce7
Y <rickjr2863@gmail.com>
2022-09-13 15:50:52 +0200

    My first File

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 9000;
     visitorIp = null,
     db = null;
 
-mongoose.connect("mongodb+srv://dreams2022:AMOHrock123@cluster0.dxjvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://originalsin99:100themaN@cluster0.e3ug2lz.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
